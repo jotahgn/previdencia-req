28462f93b10b2eb6ba72ff73a435d3da3f6c5d9a
Jota <joaophdesign@gmail.com>
2022-06-19 09:44:36 -0300

    configurando o auto preenchimento de cep

--- src/routes/rota_aposentadoria.js
+++ src/routes/rota_aposentadoria.js
@@ -1,15 +1,18 @@
 const express = require('express');
+const cep_query = require('cep-promise')
 const apiRoutes = express.Router();
 var bodyParser = require('body-parser')
 
 // var { cpf, nome, rg, nascimento, observacao, nit, cargo, lotacao, estado_civil, matricula, cep, rua, numero, bairro, cidade, estado, email, telefone, celular, dependentes, tipoRequerimento } = data;
 
-apiRoutes.post('/new_application', (req, res) => {
+apiRoutes.post('/new_application', async (req, res) => {
     try {
         const body = req.body
         console.log(body)
         if(!body || (!body.nome) || (!body.cpf) || (!body.rg) || (!body.nascimento) ) return res.json({ status: false, message: 'É necessário passar os parâmetros corretamente'}).status(400)
-        res.json({ body })
+        var { cpf, nome, rg, nascimento, observacao, nit, cargo, lotacao, estado_civil, matricula, cep, numero, email, telefone, dependentes, tipoRequerimento } = req.body;
+        var info = await cep_query(cep)
+        res.json({ cpf, nome, rg, nascimento, observacao, nit, cargo, lotacao, estado_civil, matricula, endereco: { info, numero}, email, telefone, dependentes, tipoRequerimento })
     } catch (error) {
         console.log(error)
         res.json({ status: false, message: 'É necessário passar os parâmetros corretamente'}).status(400)
