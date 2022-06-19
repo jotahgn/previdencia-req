34b3442e59f11a1b6373f343908bbe5ff779dcee
Jota <joaophdesign@gmail.com>
2022-06-19 09:30:04 -0300

    new route

--- src/app.js
+++ src/app.js
@@ -1,13 +1,16 @@
 const express = require('express');
 var hbs = require('express-handlebars');
 const apiRoutes = require('./routes/index');
+var bodyParser = require('body-parser')
 
 require('dotenv').config();
+
 
 const app = express();
 const porta = process.env.PORT || 80;
 app.use(express.urlencoded({ extended: true }));
 
+app.use(bodyParser.json())
 app.use(express.json());
 app.use(express.static(__dirname + '/public'));
 app.use('/api', apiRoutes);
--- src/routes/rota_aposentadoria.js
+++ src/routes/rota_aposentadoria.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const apiRoutes = express.Router();
+var bodyParser = require('body-parser')
+
+// var { cpf, nome, rg, nascimento, observacao, nit, cargo, lotacao, estado_civil, matricula, cep, rua, numero, bairro, cidade, estado, email, telefone, celular, dependentes, tipoRequerimento } = data;
 
 apiRoutes.post('/new_application', (req, res) => {
-    const body = req.body
-    
+    try {
+        const body = req.body
+        console.log(body)
+        if(!body || (!body.nome) || (!body.cpf) || (!body.rg) || (!body.nascimento) ) return res.json({ status: false, message: 'É necessário passar os parâmetros corretamente'}).status(400)
+        res.json({ body })
+    } catch (error) {
+        console.log(error)
+        res.json({ status: false, message: 'É necessário passar os parâmetros corretamente'}).status(400)
+    }
 });
 
 module.exports = apiRoutes;
