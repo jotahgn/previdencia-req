7ab3581771b235ef8d0e4bf5338a1e4f36abcfde
Jota <joaophdesign@gmail.com>
2022-06-20 07:19:57 -0300

    adicionado contagem de requerimentos ao frontend diretamente do banco de dados

--- src/app.js
+++ src/app.js
@@ -3,6 +3,10 @@ var hbs = require('express-handlebars');
 const apiRoutes = require('./routes/index');
 var bodyParser = require('body-parser')
 
+const pfAposentadoria = require('./database/models/aposentadoria.model')
+const pfSimples = require('./database/models/simples.model')
+
+
 require('dotenv').config();
 require('./database/db/index')
 
@@ -19,9 +23,24 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 
 
-app.get('/', (req, res) => {
-  res.render('home')
+app.get('/', async (req, res) => {
+  pfAposentadoria.count({}, function (err, count) {
+    var countAposentadoria = count;
+    pfSimples.count({}, function (err, count) {
+      var countSimples = count;
+      res.render(
+        'home',
+        {
+          countAposentadoria: countAposentadoria,
+          countSimples: countSimples
+        }
+      );
+    })
+  })
+
+
 })
+
 
 app.get('*', (req, res) => {
   res.render('404')
